e178a16c4b6f446619c36afc79b1adf187235d61
João Carlos Martins <joaocarlos12349@gmail.com>
2025-07-13 17:34:57 -0300

    Carregar Produtos 1.1

--- src/app/pages/cardapio/cardapio.component.ts
+++ src/app/pages/cardapio/cardapio.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Produto } from '../../models/produto';
+import { ProdutoService } from '../../services/produto.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cardapio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cardapio.component.html',
-  styleUrl: './cardapio.component.css'
+  styleUrl: './cardapio.component.css',
 })
 export class CardapioComponent {
+  produtos?: Produto[];
 
+  constructor(private produtoService: ProdutoService) {}
+
+  ngOnInit() {
+    this.produtoService.listar().subscribe({
+      next: (listProdutos: Produto[]) => {
+        this.produtos = listProdutos;
+      },
+    });
+  }
+
+  carregaProdutos() {
+    const cardsProdutos = document.getElementById("containerProdutos");
+
+
+  }
 }
