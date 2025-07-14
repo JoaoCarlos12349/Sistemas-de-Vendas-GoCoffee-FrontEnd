c18ac7b4baa78c587ec67adceebc76dc3de6175a
Esther Magalhaes <ecmagalhaes@verointernet.com.br>
2025-07-13 23:22:33 -0300

    adicionando + componentes

---
+++ src/app/pages/resgate-pontuacao/resgate-pontuacao.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ResgatePontuacaoService } from '../../services/resgate-pontuacao.service';
+import { ResgatePontuacao } from '../../models/resgate-pontuacao.model';
+ng add @angular/material
+
+
+@Component({
+  selector: 'app-resgate-pontuacao',
+  templateUrl: './resgate-pontuacao.component.html'
+})
+export class ResgatePontuacaoComponent implements OnInit {
+  resgates: ResgatePontuacao[] = [];
+
+  constructor(private resgatePontuacaoService: ResgatePontuacaoService) {}
+
+  ngOnInit(): void {
+    this.getAllResgates();
+  }
+
+  getAllResgates(): void {
+    this.resgatePontuacaoService.getAll().subscribe((data) => {
+      this.resgates = data;
+    });
+  }
+}
